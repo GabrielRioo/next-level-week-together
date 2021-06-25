0441d29779e8ee7dd5d5fb0422bf287a3480be10
GabrielRioo <gabrielpaivario@gmail.com>
2021-06-25 16:58:09 -0300

    Add authentication with google using firebase

--- letmeask/src/pages/NewRoom.tsx
+++ letmeask/src/pages/NewRoom.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
@@ -6,6 +8,7 @@ import "../styles/auth.scss";
 import { Button } from "../components/Button";
 
 export function NewRoom() {
+
   return (
     <div id="page-auth">
       <aside>
@@ -20,15 +23,16 @@ export function NewRoom() {
         <div className="main-content">
           <img src={logoImg} alt="Letmeask logo" />
 
-            <h2>Criar uma nova sala</h2>
+          <h2>Criar uma nova sala</h2>
 
           <form>
             <input type="text" placeholder="Nome da sala" />
             <Button type="submit">Criar sala</Button>
           </form>
-          
-            <p>Quer entrar em uma sala existente? <a href="#">Clique aqui</a> </p>
 
+          <p>
+            Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>{" "}
+          </p>
         </div>
       </main>
     </div>
