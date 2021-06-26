58e5419c340330c6156729a6757c1e7fea6a47ec
GabrielRioo <gabrielpaivario@gmail.com>
2021-06-26 13:50:07 -0300

    style of the new questions

--- letmeask/src/pages/Room.tsx
+++ letmeask/src/pages/Room.tsx
@@ -1,134 +1,151 @@
-import logoImg from '../assets/images/logo.svg';
-import { Button } from '../components/Button';
-import { RoomCode } from '../components/RoomCode';
-import '../styles/room.scss';
-import { useParams } from 'react-router-dom';
-import { FormEvent, useEffect, useState } from 'react';
-import { useAuth } from '../hooks/useAuth';
-import { database } from '../services/firebase';
+import logoImg from "../assets/images/logo.svg";
+import { Button } from "../components/Button";
+import { RoomCode } from "../components/RoomCode";
+import "../styles/room.scss";
+import { useParams } from "react-router-dom";
+import { FormEvent, useEffect, useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import { database } from "../services/firebase";
+import { Question } from "../components/Question";
 
-type FirebaseQuestions =  Record<string, {
+type FirebaseQuestions = Record<
+  string,
+  {
     author: {
-        name: string;
-        avatar: string;
-    }
+      name: string;
+      avatar: string;
+    };
     content: string;
     isAnswered: boolean;
     isHighlighted: boolean;
-}>
+  }
+>;
 
 type RoomParams = {
-    id: string;
-}
+  id: string;
+};
 
 type Question = {
-    id: string;
-    author: {
-        name: string;
-        avatar: string;
-    }
-    content: string;
-    isAnswered: boolean;
-    isHighlighted: boolean;
-}
+  id: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
+};
 
 export function Room() {
-    const params = useParams<RoomParams>();
-    const roomId = params.id;
-    const [newQuestion, setNewQuestion] = useState('');
-    const { user } = useAuth();
-    const [questions, setQuestions ]= useState<Question[]>([]);
-    const [title, setTitle] = useState(''); 
+  const params = useParams<RoomParams>();
+  const roomId = params.id;
+  const [newQuestion, setNewQuestion] = useState("");
+  const { user } = useAuth();
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [title, setTitle] = useState("");
 
-    useEffect(() => {
-        const roomRef = database.ref(`rooms/${roomId}`);
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`);
 
-        roomRef.on('value', room => {
-            // console.log(room.val());
-            const databaseRoom = room.val();
-            const fireebaseQuestions : FirebaseQuestions = databaseRoom.questions  ?? {}; 
+    roomRef.on("value", (room) => {
+      // console.log(room.val());
+      const databaseRoom = room.val();
+      const fireebaseQuestions: FirebaseQuestions =
+        databaseRoom.questions ?? {};
 
-            const parsedQuestions = Object.entries(fireebaseQuestions).map(([key, value]) => {
-                return {
-                    id: key,
-                    content: value.content,
-                    author: value.author,
-                    isHighlighted: value.isHighlighted,
-                    isAnswered: value.isAnswered,
-                }
-            })
-            // console.log(parsedQuestions);
-            setTitle(databaseRoom.title);
-            setQuestions(parsedQuestions);
-            
-        })
-    }, [roomId])
-
-    async function handleSendQuestion(event: FormEvent) {
-        event.preventDefault();
-
-        if(newQuestion.trim() === '') {
-            return;
+      const parsedQuestions = Object.entries(fireebaseQuestions).map(
+        ([key, value]) => {
+          return {
+            id: key,
+            content: value.content,
+            author: value.author,
+            isHighlighted: value.isHighlighted,
+            isAnswered: value.isAnswered,
+          };
         }
+      );
+      // console.log(parsedQuestions);
+      setTitle(databaseRoom.title);
+      setQuestions(parsedQuestions);
+    });
+  }, [roomId]);
 
-        if(!user) {
-            throw new Error('You must be logged in');
-        }
+  async function handleSendQuestion(event: FormEvent) {
+    event.preventDefault();
 
-        const question = {
-            content: newQuestion,
-            author: {
-                name: user.name,
-                avatar: user.avatar,
-            },
-            isHighlighted: false,
-            isAnswered: false
-        };
-
-        await database.ref(`/rooms/${roomId}/questions`).push(question);
-
-        setNewQuestion('');
+    if (newQuestion.trim() === "") {
+      return;
     }
 
-    return (
-        <div id="page-room">
-            <header>
-                <div className="content">
-                    <img src={logoImg} alt="Letmeask logo" />
-                    <RoomCode code={roomId}/>
-                </div>
-            </header>
+    if (!user) {
+      throw new Error("You must be logged in");
+    }
 
-            <main className="content">
-                <div className="room-title">
-                    <h1>Sala {title}</h1>
-                    { questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
-                </div>
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name,
+        avatar: user.avatar,
+      },
+      isHighlighted: false,
+      isAnswered: false,
+    };
 
-                <form onSubmit={handleSendQuestion}>
-                    <textarea
-                        placeholder="O que você quer perguntar?"
-                        onChange={event => setNewQuestion(event.target.value)}
-                        value={newQuestion}
-                    />
+    await database.ref(`/rooms/${roomId}/questions`).push(question);
 
-                    <div className="form-footer">
-                        { user ? (
-                            <div className="user-info">
-                                <img src={user.avatar} alt={user.name} />
-                                <span>{user.name}</span>
-                            </div>
-                        ) : (
-                            <span>Para enviar uma pergunta, <button>faça o seu login</button>.</span>
-                        )}
-                        
-                        <Button type="submit" disabled={!user}>Enviar Pergunta</Button>
-                    </div>
-                </form>
+    setNewQuestion("");
+  }
 
-                {JSON.stringify(questions)}
-
-            </main>
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Letmeask logo" />
+          <RoomCode code={roomId} />
         </div>
-        );
+      </header>
+
+      <main className="content">
+        <div className="room-title">
+          <h1>Sala {title}</h1>
+          {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
+        </div>
+
+        <form onSubmit={handleSendQuestion}>
+          <textarea
+            placeholder="O que você quer perguntar?"
+            onChange={(event) => setNewQuestion(event.target.value)}
+            value={newQuestion}
+          />
+
+          <div className="form-footer">
+            {user ? (
+              <div className="user-info">
+                <img src={user.avatar} alt={user.name} />
+                <span>{user.name}</span>
+              </div>
+            ) : (
+              <span>
+                Para enviar uma pergunta, <button>faça o seu login</button>.
+              </span>
+            )}
+
+            <Button type="submit" disabled={!user}>
+              Enviar Pergunta
+            </Button>
+          </div>
+        </form>
+
+        {/* {JSON.stringify(questions)} */}
+        <div className="question-list">
+          {/* Map() -Similar ao foreach porem permite retornar algo de dentro dele */}
+          {questions.map((question) => {
+            return (
+              <Question content={question.content} author={question.author} />
+            );
+          })}
+        </div>
+      </main>
+    </div>
+  );
 }
